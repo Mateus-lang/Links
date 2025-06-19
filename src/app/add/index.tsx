8056e232015de860e51c712016d302d19477f72a
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2025-06-19 00:57:04 -0300

    Adiciona componente de  formulário de adição

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -4,6 +4,7 @@ import { styles } from "./styles";
 import { colors } from "@/styles/colors";
 import { router } from "expo-router";
 import { Categories } from "@/components/categories";
+import { Input } from "@/components/input";
 
 export default function Add() {
   return (
@@ -16,7 +17,12 @@ export default function Add() {
         <Text style={styles.title}>Novo</Text>
       </View>
       <Text style={styles.label}>Selecione uma categoria</Text>
-      <Categories/>
+      <Categories />
+      <View style={styles.form}>
+        <Input placeholder="Nome" />
+        <Input placeholder="Url" />
+
+      </View>
     </View>
   );
 }
